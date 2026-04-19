ff930111429d425562b47b3d1d9401a22dd81f41
hariharan <hariharandakshnamurthy@gmail.com>
2026-04-19 12:26:40 +0530

    feat: completed Base setup

--- frenzies/src/App.jsx
+++ frenzies/src/App.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import {useState} from 'react'
 import './index.css'
+import Die from './components/Die.jsx'
 
 function App() {
+  const [dice, setDice] = useState(generateAllNewDice())
+  function generateAllNewDice(){
+    let sides = []
+    for(let i=0;i<10;i++){
+      let num = Math.floor(Math.random()*6)+1
+      sides.push(num)
+    }
+    console.log(sides)
+    return sides
+  }
+
+  const diceRoller=()=>{
+    setDice(generateAllNewDice())
+  }  
+  
   return (
     <main>
-      Main Content
+      <div className='grid-container'>
+       {
+        dice.map((die,index)=><Die key={index} value={die}/>)
+       }
+      </div>
+      <button className='roll-dice' onClick={()=>diceRoller()}>Roll the Dice</button>
     </main>
   )
 }
